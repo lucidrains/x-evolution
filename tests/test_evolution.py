efff8ff7317da9a272084b6be9ac915b983ed2e3
lucidrains <lucidrains@gmail.com>
2025-11-17 09:08:23 -0800

    some more convenience

--- tests/test_evolution.py
+++ tests/test_evolution.py
@@ -2,23 +2,23 @@ import pytest
 param = pytest.mark.parametrize
 
 import torch
+from x_mlps_pytorch import MLP
 
-@param('param_names_to_optimize', (None, ['layers.1.weight']))
+model = MLP(8, 16, 4)
+
+@param('params_to_optimize', (None, ['layers.1.weight'], [model.layers[1].weight]))
 def test_evo_strat(
-    param_names_to_optimize
+    params_to_optimize
 ):
     from random import randrange
 
     from x_evolution.x_evolution import EvoStrategy
 
-    from x_mlps_pytorch import MLP
-    model = MLP(8, 16, 4)
-
     evo_strat = EvoStrategy(
         model,
         environment = lambda model: float(randrange(100)),
         num_generations = 10,
-        param_names_to_optimize = param_names_to_optimize
+        params_to_optimize = params_to_optimize
     )
 
     evo_strat()
